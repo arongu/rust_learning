821c52cda6e455575a95b1f6b306e881e5347d13
Aron Gulyas <aron.gulyas.hu@gmail.com>
2023-02-25 10:53:07 +0100

    save() -- rust_learning -- 2023-02-25 10:53:07

--- 05_ownership/src/main.rs
+++ 05_ownership/src/main.rs
@@ -1,19 +1,43 @@
 fn main() {
     let x: i32 = 5; //
-    let y: i32 = x; // it works because it has a Copy trait, which allows it to be copied, more on that later
+    let _y: i32 = x; // it works because it has a Copy trait, which allows it to be copied, more on that later
+    let z: i32 = 5;
 
-    let s: String = String::from("own_me");
-    takes_ownership(s);
-    // println!("{}" , s); // this would fail, because the variable does not own anything
 
-    // let z: i32 = 5;
-    // makes_copy(some_integer: x);
+
+    // ownership changes
+    let s0: String = String::from("own_me");
+    takes_ownership(s0);
+    // println!("s = {}" , s); // this would fail, because the variable does not own anything
+
+    // ownership does not change, because integer has copy trait
+    makes_copy(z);
+    println!("z = {}", z);
+
+    // gives
+    let s1 = gives_ownership();
+    println!("s1 = {}", s1);
+    // s1 = String::from("whaaaat?"); // immutable variables cannot own new values, but their
+                                      // ownership can be revoked by passing it to a function... !!!!
+    let s2 = takes_and_gives_ownership(s1);
+    println!("s2 = {}", s2);
 }
 
-fn takes_ownership(some_string:String) {
-    println!("owning: {}", some_string);
-}
-
-fn makes_copy(some_integer: i32) {
+fn makes_copy(some_integer: i32) { // because integer has the copy trait?
     println!("{}", some_integer);
 }
+
+fn takes_ownership(some_string: String) {
+    println!("taking ownership of this String: {}", some_string);
+}
+
+fn gives_ownership() -> String {
+    let some_string: String = String::from("Now you own this string!");
+    some_string
+}
+
+fn takes_and_gives_ownership(some_string: String) -> String {
+    println!("I am going to give it back: {}", some_string);
+    some_string
+}
+
