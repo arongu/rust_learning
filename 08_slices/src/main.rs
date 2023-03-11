d5aeb279dab9a521fc5135cc6c455004c46af192
Aron Gulyas <aron.gulyas.hu@gmail.com>
2023-03-11 11:02:52 +0100

    cargo-fmt

--- 08_slices/src/main.rs
+++ 08_slices/src/main.rs
@@ -2,7 +2,7 @@ fn one() {
     let my_string: String = String::from("This is my string");
     let my_slice = &my_string[0..];
 
-    for ( i, v ) in my_slice.chars().enumerate() {
+    for (i, v) in my_slice.chars().enumerate() {
         println!("index = {}, value = {}", i, v);
     }
 }
@@ -20,7 +20,7 @@ fn two() {
 fn print_range(string: &str, beg: usize, end: usize) {
     if end <= string.len() {
         let my_slice = &string[beg..end];
-        for ( index, char ) in my_slice.chars().enumerate() {
+        for (index, char) in my_slice.chars().enumerate() {
             println!("index = {}, value = '{}'", index, char);
         }
     }
@@ -28,16 +28,15 @@ fn print_range(string: &str, beg: usize, end: usize) {
 
 fn print_largest(numbers: &[i32]) {
     if !numbers.is_empty() {
-        let mut max : i32 = numbers[0];
+        let mut max: i32 = numbers[0];
 
-        for ( _, value) in numbers.iter().enumerate() {
+        for (_, value) in numbers.iter().enumerate() {
             if value.gt(&max) {
                 max = *value; //max = i32::from(*value);
             }
         }
 
         println!("The largest number in the array is: {}", max);
-
     } else {
         println!("The array is empty.");
     }
@@ -48,6 +47,6 @@ fn main() {
     print_range(&my_string, 0, 0);
     print_range(&my_string, 0, my_string.len());
 
-    let my_numbers: [i32;4] = [ 10, 88, 1, -12 ];
+    let my_numbers: [i32; 4] = [10, 88, 1, -12];
     print_largest(&my_numbers);
 }
