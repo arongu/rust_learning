9c9f35fa701da26d5ade0b2252d8ff95f974525c
Aron Gulyas <aron.gulyas.hu@gmail.com>
2023-03-24 08:42:09 +0100

    first function for vector

--- 12_common_collections/src/a/mod.rs
+++ 12_common_collections/src/a/mod.rs
@@ -1,26 +1,28 @@
-pub fn one(){
-    let a: [i32; 3] = [1, 2, 3];      // array
+fn get_from_vec(vec: &Vec<i32>, index: usize) {
+    match vec.get(index) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no {} element.", index)
+    }
+}
 
-    let mut v: Vec<i32> = Vec::new(); // it can grow in size
+pub fn one(){
+    let a: [i32; 3] = [1, 2, 3]; // array
+    let mut v: Vec<i32> = Vec::new(); // vector created on the heap, it can grow its size
     v.push(1);
     v.push(2);
     v.push(3);
 
-    let v2: Vec<i32> = vec![1, 2, 3]; //
-    let third: &i32 = &v[2];
-    println!("Thi third element is {}", third);
+    let v2: Vec<i32> = vec![1, 2, 3]; // macro to create vector from values
+    let third: &i32 = &v[2]; // v[20] could be out of bound, because this is a vector, we do not know its size
+                             // index 20 can be invalid
+    println!("The third element is {}", third);
 
-    v.get(2) {
-
-    }
-
-    match v.get(2) {
+    // we are using match to be sure what we are accessing is inbound
+    match v.get(2) { // accessing element in the vector
         Some(third) => println!("The third element is {}", third),
         None => println!("There is no third element.")
     }
 
-    match v.get(10) {
-        Some(third) => println!("The third element is {}", third),
-        None => println!("There is no third element.")
-    }
+    get_from_vec(&v, 1);
+    get_from_vec(&v, 10);
 }
