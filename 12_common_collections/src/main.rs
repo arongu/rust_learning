09cbaa37bf36225bb8974b71c18f1b36d420a7bd
Aron Gulyas <aron.gulyas.hu@gmail.com>
2023-03-25 11:38:37 +0100

    12 refactored: better module names

--- 12_common_collections/src/main.rs
+++ 12_common_collections/src/main.rs
@@ -1,18 +1,20 @@
-mod a;
-mod b;
-mod c;
+mod a_vectors;
+mod b_utf8;
+mod c_hashmaps;
 
 fn main() {
     // part 1
-    a::one();
-    a::two();
-    a::third();
+    a_vectors::one();
+    a_vectors::two();
+    a_vectors::third();
     println!();
+
     // part 2
-    b::one();
-    b::two();
+    b_utf8::one();
+    b_utf8::two();
+
     // part 3
-    c::one();
-    c::two();
-    c::three();
+    c_hashmaps::one();
+    c_hashmaps::two();
+    c_hashmaps::three();
 }
